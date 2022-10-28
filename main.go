12881f998a19d1156525f8a6786f23f5f5a41196
ahmos <kamaboko_0228@icloud.com>
2022-10-28 20:46:48 +0900

    firestoreなしの状態で動くようにした。HAMADAと朱雀の学籍番号でテストしました。

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"net/http"
+	"gakumon_go/controller"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "HelloWorld")
-	})
+	router.GET("/gakumon_list", controller.GetGakumonList)
 	router.Run(":8080")
 }
