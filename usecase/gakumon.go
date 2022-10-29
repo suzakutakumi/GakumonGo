c0bcb8e188f6b2a77a06a66dccc8a5773be1d167
suzakutakumi <suzakutakumi3@gmail.com>
2022-10-30 02:07:02 +0900

    学モンを登録するusecase作成

--- usecase/gakumon.go
+++ usecase/gakumon.go
@@ -1,15 +1,21 @@
 package usecase
 
 import (
+	"fmt"
 	"gakumon_go/model"
+	"mime/multipart"
 )
 
 type GakumonUsecase interface {
 	GetGakumonList() ([]model.GakumonID, error)
+	RegisterNewGakumon(model.Gakumon, multipart.File, multipart.FileHeader) (model.Gakumon, error)
 }
 
 type GakumonRepository interface {
-	FetchAllGakumonId() ([]model.GakumonID, error)
+	FetchAllGakumonID() ([]model.GakumonID, error)
+	InsertGakumonInfo(model.Gakumon) (model.Gakumon, error)
+	UploadGakumonImage(multipart.File, multipart.FileHeader) (string, error)
+	DeleteGakumonImage(string) (string, error)
 }
 
 type gakumonUsecaseImpl struct {
@@ -21,5 +27,24 @@ func NewGakumonUsecase(r GakumonRepository) *gakumonUsecaseImpl {
 }
 
 func (u gakumonUsecaseImpl) GetGakumonList() ([]model.GakumonID, error) {
-	return u.r.FetchAllGakumonId()
+	return u.r.FetchAllGakumonID()
+}
+
+func (u gakumonUsecaseImpl) RegisterNewGakumon(g model.Gakumon, img multipart.File, img_info multipart.FileHeader) (model.Gakumon, error) {
+	path, err := u.r.UploadGakumonImage(img, img_info)
+	if err != nil {
+		return model.Gakumon{}, err
+	}
+
+	g.ImageURL = &path
+
+	gakumon, err := u.r.InsertGakumonInfo(g)
+	if err != nil {
+		if _, derr := u.r.DeleteGakumonImage(img_info.Filename); derr != nil {
+			return model.Gakumon{}, fmt.Errorf("%s&%s", err.Error(), derr.Error())
+		}
+		return model.Gakumon{}, err
+	}
+
+	return gakumon, nil
 }
