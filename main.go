14a292663ef829d69f7a45192d3c96c9f0ca87cc
suzakutakumi <suzakutakumi3@gmail.com>
2022-10-30 17:47:36 +0900

    mainの修正

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"gakumon_go/controller"
 	"gakumon_go/repository"
 	"gakumon_go/usecase"
@@ -13,33 +14,46 @@ import (
 
 func main() {
 	if err := godotenv.Load(".env"); err != nil {
-		log.Fatal("環境変数がない")
+		log.Fatalf("環境変数がない:%s", err.Error())
 	}
 	keypath := os.Getenv("ACCOUNT_KEY_JSON_PATH")
 
-	client, ctx, err := repository.InitFirebase(keypath)
-	if err != nil {
-		log.Fatal("firebaseが初期化されなかった")
-	}
-	defer client.Close()
+	ctx := context.Background()
 
-	r := repository.GakumonRepositoryImpl{Client: client, Ctx: ctx}
+	app, err := repository.InitFirebase(ctx, keypath)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	storeCli, err := repository.InitFirestore(ctx, app)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer storeCli.Close()
+
+	storageCli, err := repository.InitCloudStorage(ctx, app)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	r := repository.GakumonRepositoryImpl{Firestore: storeCli, Storage: storageCli, Ctx: ctx}
 	u := usecase.NewGakumonUsecase(r)
 	gakumonctrl := controller.NewGakumonController(u)
 
 	questionctrl := controller.NewQuestionControllerImpl(
 		usecase.NewQuestionUsecase(
-			repository.QuestionRepositoryImpl{Client: client, Ctx: ctx},
+			repository.QuestionRepositoryImpl{Client: storeCli, Ctx: ctx},
 		),
 	)
 
 	answerctrl := controller.NewAnswerControllerImpl(
 		usecase.NewAnswerUsecase(
-			repository.AnswerRepositoryImpl{Client: client, Ctx: ctx},
+			repository.AnswerRepositoryImpl{Client: storeCli, Ctx: ctx},
 		),
 	)
 	router := gin.Default()
 	router.GET("/gakumon_list", gakumonctrl.GetGakumonList)
+	router.POST("/gakumon", gakumonctrl.RegisterNewGakumon)
 	router.GET("/gakumon/:gakumon_id/question", questionctrl.GetQuestion)
 	router.POST("/gakumon/:gakumon_id/answer", answerctrl.CheckCollect)
 
