c9bd27ee8a24404251ebc4200d7893f086997a27
suzakutakumi <suzakutakumi3@gmail.com>
2022-10-30 17:44:40 +0900

    usecaseの修正

--- usecase/gakumon.go
+++ usecase/gakumon.go
@@ -8,14 +8,16 @@ import (
 
 type GakumonUsecase interface {
 	GetGakumonList() ([]model.GakumonID, error)
-	RegisterNewGakumon(model.Gakumon, multipart.File, multipart.FileHeader) (model.Gakumon, error)
+	RegisterNewGakumon(model.Gakumon, model.QandA, *multipart.File, *string) error
 }
 
 type GakumonRepository interface {
 	FetchAllGakumonID() ([]model.GakumonID, error)
-	InsertGakumonInfo(model.Gakumon) (model.Gakumon, error)
-	UploadGakumonImage(multipart.File, multipart.FileHeader) (string, error)
-	DeleteGakumonImage(string) (string, error)
+	InsertGakumonInfo(model.Gakumon) error
+	InsertQandA(model.QandA) error
+	DeleteQandA(string) error
+	UploadGakumonImage(multipart.File, string) (string, error)
+	DeleteGakumonImage(string) error
 }
 
 type gakumonUsecaseImpl struct {
@@ -30,21 +32,37 @@ func (u gakumonUsecaseImpl) GetGakumonList() ([]model.GakumonID, error) {
 	return u.r.FetchAllGakumonID()
 }
 
-func (u gakumonUsecaseImpl) RegisterNewGakumon(g model.Gakumon, img multipart.File, img_info multipart.FileHeader) (model.Gakumon, error) {
-	path, err := u.r.UploadGakumonImage(img, img_info)
-	if err != nil {
-		return model.Gakumon{}, err
+func (u gakumonUsecaseImpl) RegisterNewGakumon(g model.Gakumon, qanda model.QandA, img *multipart.File, img_name *string) error {
+	if err := u.r.InsertQandA(qanda); err != nil {
+		return err
 	}
 
-	g.ImageURL = &path
-
-	gakumon, err := u.r.InsertGakumonInfo(g)
-	if err != nil {
-		if _, derr := u.r.DeleteGakumonImage(img_info.Filename); derr != nil {
-			return model.Gakumon{}, fmt.Errorf("%s&%s", err.Error(), derr.Error())
+	if img == nil || img_name == nil {
+		if err := u.r.InsertGakumonInfo(g); err != nil {
+			if dqerr := u.r.DeleteQandA(qanda.GakumonID); err != nil {
+				return fmt.Errorf("%s&%s", err.Error(), dqerr.Error())
+			}
+			return err
 		}
-		return model.Gakumon{}, err
+	} else {
+		path, err := u.r.UploadGakumonImage(*img, *img_name)
+		if err != nil {
+			return err
+		}
+
+		g.ImageURL = &path
+
+		if err := u.r.InsertGakumonInfo(g); err != nil {
+			errMsg := err.Error()
+			if dgerr := u.r.DeleteGakumonImage(*img_name); dgerr != nil {
+				errMsg += "&" + dgerr.Error()
+			}
+			if dqerr := u.r.DeleteQandA(qanda.GakumonID); err != nil {
+				errMsg += "&" + dqerr.Error()
+			}
+			return fmt.Errorf(errMsg)
+		}
 	}
 
-	return gakumon, nil
+	return nil
 }
