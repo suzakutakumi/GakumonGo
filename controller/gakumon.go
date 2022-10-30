f414d5d76f4c0bf94a91292cf5dca33ec7176ed5
suzakutakumi <suzakutakumi3@gmail.com>
2022-10-30 17:47:24 +0900

    controller層の実装

--- controller/gakumon.go
+++ controller/gakumon.go
@@ -1,8 +1,11 @@
 package controller
 
 import (
+	"encoding/json"
+	"gakumon_go/model"
 	"gakumon_go/usecase"
 	"log"
+	"mime/multipart"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +13,7 @@ import (
 
 type GakumonController interface {
 	GetGakumonList()
+	RegisterNewGakumon()
 }
 
 type gakumonControllerImpl struct {
@@ -28,4 +32,50 @@ func (c gakumonControllerImpl) GetGakumonList(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, l)
+}
+
+func (c gakumonControllerImpl) RegisterNewGakumon(ctx *gin.Context) {
+	var gakumon model.Gakumon
+	gakumonJson := ctx.Request.FormValue("gakumon")
+	if err := json.Unmarshal([]byte(gakumonJson), &gakumon); err != nil {
+		ctx.Status(http.StatusBadRequest)
+		return
+	}
+
+	var qanda model.QandA
+	qandaJson := ctx.Request.FormValue("qanda")
+	if err := json.Unmarshal([]byte(qandaJson), &qanda); err != nil {
+		log.Println(err.Error())
+		ctx.Status(http.StatusBadRequest)
+		return
+	}
+
+	var imgPtr *multipart.File = nil
+	var imgNamePtr *string = nil
+
+	img, imgInfo, err := ctx.Request.FormFile("image")
+	if err == nil {
+		imgName := gakumon.GakumonID
+
+		imgType := imgInfo.Header.Get("Content-Type")
+		if imgType == "image/jpeg" {
+			imgName += ".jpg"
+		} else if imgType == "image/png" {
+			imgName += ".png"
+		} else {
+			log.Println(err.Error())
+			ctx.Status(http.StatusBadRequest)
+			return
+		}
+
+		imgPtr = &img
+		imgNamePtr = &imgName
+	}
+	if err := c.u.RegisterNewGakumon(gakumon, qanda, imgPtr, imgNamePtr); err != nil {
+		log.Println(err.Error())
+		ctx.Status(http.StatusBadRequest)
+		return
+	}
+
+	ctx.Status(http.StatusCreated)
 }
